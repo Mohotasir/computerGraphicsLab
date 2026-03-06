b0d397bfc46c70dde4897c2cc7e408f1a4e32ff4
Md. Mohotasir Al Mamun <87077964+Mohotasir@users.noreply.github.com>
2026-03-06 16:54:46 +0600

    Refactor clock drawing and update window title

--- D_test.cpp
+++ D_test.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int centerX = 320, centerY = 240;
 int radius = 150;
 
+/* -------- Plot Pixel -------- */
 void plot(int x, int y)
 {
     glBegin(GL_POINTS);
@@ -13,15 +14,15 @@ void plot(int x, int y)
     glEnd();
 }
 
+/* -------- Midpoint Circle Algorithm -------- */
 void midPointCircle(int xc, int yc, int r)
 {
     int x = 0;
     int y = r;
-    int p = 1 - r;   // Initial decision parameter
+    int p = 1 - r;
 
     while (x <= y)
     {
-        // 8-way symmetry
         plot(xc + x, yc + y);
         plot(xc - x, yc + y);
         plot(xc + x, yc - y);
@@ -33,12 +34,10 @@ void midPointCircle(int xc, int yc, int r)
 
         if (p < 0)
         {
-            // East pixel
             p = p + 2 * x + 3;
         }
         else
         {
-            // South-East pixel
             p = p + 2 * x - 2 * y + 5;
             y--;
         }
@@ -47,60 +46,113 @@ void midPointCircle(int xc, int yc, int r)
     }
 }
 
+/* -------- Bresenham Line Algorithm (From Your Code) -------- */
 void drawLine(int x1, int y1, int x2, int y2)
 {
-    float dx = x2 - x1;
-    float dy = y2 - y1;
-    float steps = max(abs(dx), abs(dy));
+    int dx, dy, i, e;
+    int incx, incy, inc1, inc2;
+    int x, y;
 
-    float xInc = dx / steps;
-    float yInc = dy / steps;
+    dx = x2 - x1;
+    dy = y2 - y1;
 
-    float x = x1;
-    float y = y1;
+    if (dx < 0) dx = -dx;
+    if (dy < 0) dy = -dy;
 
-    for (int i = 0; i <= steps; i++)
+    incx = 1;
+    if (x2 < x1) incx = -1;
+
+    incy = 1;
+    if (y2 < y1) incy = -1;
+
+    x = x1;
+    y = y1;
+
+    if (dx > dy)
     {
-        plot(round(x), round(y));
-        x += xInc;
-        y += yInc;
+        plot(x, y);
+        e = 2 * dy - dx;
+        inc1 = 2 * (dy - dx);
+        inc2 = 2 * dy;
+
+        for (i = 0; i < dx; i++)
+        {
+            if (e >= 0)
+            {
+                y += incy;
+                e += inc1;
+            }
+            else
+            {
+                e += inc2;
+            }
+
+            x += incx;
+            plot(x, y);
+        }
+    }
+    else
+    {
+        plot(x, y);
+        e = 2 * dx - dy;
+        inc1 = 2 * (dx - dy);
+        inc2 = 2 * dx;
+
+        for (i = 0; i < dy; i++)
+        {
+            if (e >= 0)
+            {
+                x += incx;
+                e += inc1;
+            }
+            else
+            {
+                e += inc2;
+            }
+
+            y += incy;
+            plot(x, y);
+        }
     }
 }
 
-/* ---------- Draw Clock ---------- */
+/* -------- Draw Clock -------- */
 void drawClock()
 {
-    // Outer Circle
+    // Draw outer circle
     midPointCircle(centerX, centerY, radius);
 
-    // 4 Main Tick Marks
-    drawLine(centerX, centerY + radius, centerX, centerY + radius - 20); // 12
-    drawLine(centerX + radius, centerY, centerX + radius - 20, centerY); // 3
-    drawLine(centerX, centerY - radius, centerX, centerY - radius + 20); // 6
-    drawLine(centerX - radius, centerY, centerX - radius + 20, centerY); // 9
+    // 12 o'clock mark
+    drawLine(centerX, centerY + radius, centerX, centerY + radius - 20);
 
-    // Clock Hands (Static Position like image)
-    // Hour hand
-    drawLine(centerX, centerY, centerX + 40, centerY + 70);
+    // 3 o'clock mark
+    drawLine(centerX + radius, centerY, centerX + radius - 20, centerY);
 
-    // Minute hand
-    drawLine(centerX, centerY, centerX + 80, centerY - 60);
+    // 6 o'clock mark
+    drawLine(centerX, centerY - radius, centerX, centerY - radius + 20);
 
-    // Second hand
-    drawLine(centerX, centerY, centerX + 60, centerY + 20);
+    // 9 o'clock mark
+    drawLine(centerX - radius, centerY, centerX - radius + 20, centerY);
+
+    // Clock hands
+    drawLine(centerX, centerY, centerX + 40, centerY + 70);  // hour
+    drawLine(centerX, centerY, centerX + 80, centerY - 60);  // minute
+    drawLine(centerX, centerY, centerX + 60, centerY + 20);  // second
 }
 
-/* ---------- Display ---------- */
+/* -------- Display -------- */
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(0, 0, 0);
     glPointSize(2);
+
     drawClock();
+
     glFlush();
 }
 
-
+/* -------- Initialization -------- */
 void init()
 {
     glClearColor(1, 1, 1, 1);
@@ -109,13 +161,13 @@ void init()
     gluOrtho2D(0, 640, 0, 480);
 }
 
-/* ---------- Main ---------- */
+/* -------- Main Function -------- */
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(640, 480);
-    glutCreateWindow("Analog Clock using Midpoint Circle");
+    glutCreateWindow("Analog Clock using Midpoint + Bresenham");
 
     init();
     glutDisplayFunc(display);
