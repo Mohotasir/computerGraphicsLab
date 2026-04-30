c8fa41922cd96b7ca20d2e877dfb8daf1f913aef
Md. Mohotasir Al Mamun <87077964+Mohotasir@users.noreply.github.com>
2026-04-30 15:42:18 +0600

    Refactor drawLine function using Bresenham's algorithm

--- LabMid_D.cpp
+++ LabMid_D.cpp
@@ -24,25 +24,63 @@ void getPixel(int x, int y, float pixels[3]) {
     glReadPixels(x, y, 1, 1, GL_RGB, GL_FLOAT, pixels);
 }
 
+void draw_pixel(int x, int y)
+{
+	glBegin(GL_POINTS);
+	glVertex2i(x, y);
+	glEnd();
+}
 // -------- BRESENHAM LINE --------
 void drawLine(int x1, int y1, int x2, int y2) {
-    int dx = abs(x2 - x1);
-    int dy = abs(y2 - y1);
-    int sx = (x1 < x2) ? 1 : -1;
-    int sy = (y1 < y2) ? 1 : -1;
-    int err = dx - dy;
-
-    while (1) {
-        glBegin(GL_POINTS);
-        glVertex2i(x1, y1);
-        glEnd();
-
-        if (x1 == x2 && y1 == y2) break;
-
-        int e2 = 2 * err;
-        if (e2 > -dy) { err -= dy; x1 += sx; }
-        if (e2 < dx) { err += dx; y1 += sy; }
-    }
+	int dx, dy, i, e;
+	int incx, incy, inc1, inc2;
+	int x, y;
+	dx = x2 - x1;
+	dy = y2 - y1;
+	if (dx < 0) dx = -dx;
+	if (dy < 0) dy = -dy;
+	incx = 1;
+	if (x2 < x1) incx = -1;
+	incy = 1;
+	if (y2 < y1) incy = -1;
+	x = x1; y = y1;
+	if (dx > dy) {
+		draw_pixel(x, y);
+		e = 2 * dy - dx;
+		inc1 = 2 * (dy - dx);
+		inc2 = 2 * dy;
+		for (i = 0; i < dx; i++)
+		{
+			if (e >= 0)
+			{
+				y += incy;
+				e += inc1;
+			}
+			else
+				e += inc2;
+			x += incx;
+			draw_pixel(x, y);
+		}
+	}
+	else
+	{
+		draw_pixel(x, y);
+		e = 2 * dx - dy;
+		inc1 = 2 * (dx - dy);
+		inc2 = 2 * dx;
+		for (i = 0; i < dy; i++)
+		{
+			if (e >= 0)
+			{
+				x += incx;
+				e += inc1;
+			}
+			else
+				e += inc2;
+			y += incy;
+			draw_pixel(x, y);
+		}
+	}
 }
 
 // -------- TRIANGLE --------
